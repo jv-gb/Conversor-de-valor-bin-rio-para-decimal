d77cd8f8f09134f0b8dfb03a16f12638d4f5c62b
João Victor Gomes <95916167+jv-gb@users.noreply.github.com>
2022-05-30 10:55:00 -0300

    Add files via upload
    Agora quando colocado um número diferente de 0 e 1, não aparece mais NaN na tela e sim uma mensagem para inserir apenas 0 e 1 no input

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ function conversor(input) {
   let resultado = document.getElementById("resultado")
   //o parseInt está convertendo os valores do input para números decimais
   let valorConvertido = parseInt(input.value, 2)
-  resultado.innerHTML = valorConvertido
+ //O condicional agora retorna uma mensagem caso os caracteres inseridos não sejam 0 ou 1
+  if (isNaN(valorConvertido)) {
+   return resultado.innerHTML = "Favor inserir apenas 0 e 1"
+  }else {
+   return resultado.innerHTML = valorConvertido
+  }
 }
 
 //essa função esconde o texto do input ao clicar nele para digitar
-function hide(valor){
-valor.value = null
+function hide(valor) {
+  valor.value = null
 }
+
+
+
